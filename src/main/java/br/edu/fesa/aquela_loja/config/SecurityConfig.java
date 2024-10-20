36b95a20755bd850720faa83b08e091b0abba6b5
Pedro <pedro_henrique_dsr@hotmail.com>
2024-10-20 17:16:28 -0300

    Definindo melhor as rotas

---
+++ src/main/java/br/edu/fesa/aquela_loja/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package br.edu.fesa.aquela_loja.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.defaultSuccessUrl("/", true))
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .build();
+    }
+}
---
+++ src/main/java/br/edu/fesa/aquela_loja/controller/IndexController.java
@@ -0,0 +1,18 @@
+package br.edu.fesa.aquela_loja.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/"})
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/products")
+    public String products() {
+        return "product_details";
+    }
+}
