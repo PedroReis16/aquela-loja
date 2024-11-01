d918219d84e4e1c805cabf7a5663cd98f8d84ff3
Pedro <pedro.henrique@mobilemed.com.br>
2024-11-01 20:24:41 -0300

    - Adição da logo para login - Inicio da UI de login

---
+++ src/main/resources/static/js/page_scripts/login.js
@@ -0,0 +1,13 @@
+const passwordInput = document.getElementById("passwordInput");
+const toggleButton = document.querySelector(".toggle-password");
+
+toggleButton.addEventListener("click", () => {
+    console.log("Teste");
+    // if (passwordInput.type === "password") {
+    //     passwordInput.type = "text";
+    //     toggleButton.textContent = "🙈"; // Altera o ícone para "ocultar"
+    // } else {
+    //     passwordInput.type = "password";
+    //     toggleButton.textContent = "👁️"; // Altera o ícone para "visualizar"
+    // }
+});
