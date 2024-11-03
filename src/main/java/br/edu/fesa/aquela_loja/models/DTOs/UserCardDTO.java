d001b46544bf2540a9b8f312ab0235687a452c6d
Pedro <pedro_henrique_dsr@hotmail.com>
2024-11-02 22:11:55 -0300

    Mapeando os objetos para dtos de endereço e cartão

--- src/main/java/br/edu/fesa/aquela_loja/models/DTOs/UserCardDTO.java
+++ src/main/java/br/edu/fesa/aquela_loja/models/DTOs/UserCardDTO.java
@@ -1,5 +1,13 @@
 package br.edu.fesa.aquela_loja.models.DTOs;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter 
+@Setter
 public class UserCardDTO {
-    
+        private String number;
+        private String name;
+        private String expirationDate;
+        private String cvv;   
 }
