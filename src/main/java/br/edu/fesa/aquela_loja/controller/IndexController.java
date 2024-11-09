0f3f089574c498bab2d657254b36a68705f7f7eb
Pedro <pedro_henrique_dsr@hotmail.com>
2024-11-09 01:23:30 -0300

    Limpando a classe controller

--- src/main/java/br/edu/fesa/aquela_loja/controller/IndexController.java
+++ src/main/java/br/edu/fesa/aquela_loja/controller/IndexController.java
@@ -1,22 +1,8 @@
 package br.edu.fesa.aquela_loja.controller;
 
-import br.edu.fesa.aquela_loja.controller.dto.RegistrationDto;
-import br.edu.fesa.aquela_loja.models.entity.AddressModel;
-import br.edu.fesa.aquela_loja.models.entity.AppUserModel;
-import br.edu.fesa.aquela_loja.repository.IAddressRepository;
-import br.edu.fesa.aquela_loja.repository.IAppUserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class IndexController {
-
-   
 
 }
