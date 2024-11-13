3b21e85d236ca900ab4846814b6af8b2b6acc198
Pedro <pedro_henrique_dsr@hotmail.com>
2024-11-12 21:44:07 -0300

    Deixando um título no card de endereços

--- src/main/resources/static/js/components/editable-address-card.js
+++ src/main/resources/static/js/components/editable-address-card.js
@@ -3,48 +3,66 @@ class EditableAddressCard extends HTMLElement {
         super();
         const shadow = this.attachShadow({ mode: 'open' });
 
-        const content = `
-            <div class="address-card">
-                <div class="card-content">
-                    <h3 id="address-identification"></h3>
-                    <p id="street"></p>
-                    <p id="number-string"></p>
-                    <p id="details"></p>
-                    <p id="reference"></p>
-                </div>
-                <div class="card-actions">
-                    <button type="button" class="edit-address-btn">Editar</button>
-                </div>
-            </div>
-        `;
+        // Definindo os atributos do endereço
+        const attributes = ['address-identification', 'street', 'number-string', 'details', 'reference'];
 
+        // Criando o elemento base do card
+        const card = document.createElement('div');
+        card.setAttribute('class', 'address-card');
+
+        // Criando a DIV que irá conter o conteúdo do card
+        const cardContent = document.createElement('div');
+        cardContent.setAttribute('class', 'card-content');
+
+        // Criando e adicionando os elementos de conteúdo
+        attributes.forEach(attr => {
+            const element = document.createElement(attr === 'address-identification' ? 'h3' : 'p');
+            element.setAttribute('id', attr);
+            cardContent.appendChild(element);
+        });
+
+        // Definindo o a div de ações do card e o botão de edição
+        const cardActions = document.createElement('div');
+        cardActions.setAttribute('class', 'card-actions');
+        const editAddressButton = document.createElement('button');
+        editAddressButton.setAttribute('type', 'button');
+        editAddressButton.setAttribute('class', 'edit-address-btn');
+        editAddressButton.textContent = 'Editar';
+
+        // Juntando as partes do conteúdo na DIV de conteúdo
+        cardActions.appendChild(editAddressButton);
+
+        // Juntando tudo ao componente principal
+        card.appendChild(cardContent);
+        card.appendChild(cardActions);
+
+        // Definindo o estilo do card
         const style = document.createElement('style');
-
         style.textContent = `
             .address-card {
-                    border: 1px solid #ccc;
-                    padding: 16px;
-                    margin: 8px 0;
-                    border-radius: 8px;
-                    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-                }
-                .card-content {
-                    margin-bottom: 16px;
-                }
-                .card-actions {
-                    text-align: right;
-                }
-                .edit-address-btn {
-                    background-color: #007bff;
-                    color: white;
-                    border: none;
-                    padding: 8px 16px;
-                    border-radius: 4px;
-                    cursor: pointer;
-                }`;
+                border: 1px solid #ccc;
+                padding: 16px;
+                margin: 8px 0;
+                border-radius: 8px;
+                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            }
+            .card-content {
+                margin-bottom: 16px;
+            }
+            .card-actions {
+                text-align: right;
+            }
+            .edit-address-btn {
+                background-color: #007bff;
+                color: white;
+                border: none;
+                padding: 8px 16px;
+                border-radius: 4px;
+                cursor: pointer;
+            }`;
 
         shadow.appendChild(style);
-        shadow.innerHTML += content;
+        shadow.appendChild(card);
     }
 
     static get observedAttributes() {
@@ -52,7 +70,7 @@ class EditableAddressCard extends HTMLElement {
     }
 
     attributeChangedCallback(name, _, newValue) {
-        this.shadowRoot.getElementById(name.replace(/-/g, '')).textContent = newValue;
+        this.shadowRoot.getElementById(name).textContent = newValue;
     }
 }
 
