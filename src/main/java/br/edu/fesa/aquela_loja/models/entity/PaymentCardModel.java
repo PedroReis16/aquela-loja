e47353f22c4424deb3507de692424783fa08584d
Pedro <pedro_henrique_dsr@hotmail.com>
2024-11-15 00:37:02 -0300

    Inicio da página de cartões

--- src/main/java/br/edu/fesa/aquela_loja/models/entity/PaymentCardModel.java
+++ src/main/java/br/edu/fesa/aquela_loja/models/entity/PaymentCardModel.java
@@ -1,6 +1,13 @@
 package br.edu.fesa.aquela_loja.models.entity;
 
-import jakarta.persistence.*;
+import br.edu.fesa.aquela_loja.models.enums.CardTypeEnum;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.NoArgsConstructor;
 
 @Table(name = "wallet")
@@ -12,9 +19,15 @@ public class PaymentCardModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String type;
+    private CardTypeEnum type;
+
+    private String holderName;
 
     private String number;
+
+    private String expirationDate;
+
+    private int cvv;
 
     @ManyToOne
     @JoinColumn(name = "userId")
