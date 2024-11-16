f25fc49a24e3999fa50ad8cabdf9cf28d84699d5
Pedro <pedro_henrique_dsr@hotmail.com>
2024-11-16 13:31:51 -0300

    Visibilidade dos componentes do cabeçalho nas páginas de login e cadastro

--- src/main/resources/static/js/fragments_scripts/header.js
+++ src/main/resources/static/js/fragments_scripts/header.js
@@ -1,11 +1,26 @@
 const currentRoute = window.location.pathname;
-
 const menuBtn = document.querySelector('#menuToggle');
-
 const component = document.getElementById('menuContainer');
 
+
+//Listagem de categoriass 
 menuBtn.addEventListener('click', function () {
     component.classList.toggle('fullscreen');
     document.body.classList.toggle('no-scroll', component.classList.contains('fullscreen'));
 
 });
+
+
+if (currentRoute == '/login' || currentRoute == '/cadastro') {
+    const occultableItems = document.querySelectorAll('.occultable');
+    const header = document.querySelector('.header');
+    const logo = document.querySelector('.logo-container');
+
+    occultableItems.forEach(item => {
+        item.setAttribute('style', 'display: none');
+    });
+
+    header.setAttribute('style', 'height: 72px');
+    logo.setAttribute('style', 'justify-content: center');
+
+}
