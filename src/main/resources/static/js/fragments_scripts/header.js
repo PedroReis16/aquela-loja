aabbac5cce1c3537cdf915d122a67a1d81404ef5
Pedro <pedro_henrique_dsr@hotmail.com>
2024-11-16 02:31:51 -0300

    - Ajustando a importação do script Js do cabeçalho - Botões de acesso no menu mobile

--- src/main/resources/static/js/fragments_scripts/header.js
+++ src/main/resources/static/js/fragments_scripts/header.js
@@ -2,7 +2,7 @@ const currentRoute = window.location.pathname;
 
 const menuBtn = document.querySelector('#menuToggle');
 
-const component = document.getElementById('departamentList');
+const component = document.getElementById('menuContainer');
 
 menuBtn.addEventListener('click', function () {
     component.classList.toggle('fullscreen');
