b640f7d676cd79b46d10c462f2e57e623f73a163
EForamiglio <erik.foramiglio.vieira@gmail.com>
2024-11-20 19:20:52 -0300

    Barra de pesquisa e mais produtos na index

--- src/main/java/br/edu/fesa/aquela_loja/repository/IProductRepository.java
+++ src/main/java/br/edu/fesa/aquela_loja/repository/IProductRepository.java
@@ -2,6 +2,7 @@ package br.edu.fesa.aquela_loja.repository;
 
 import br.edu.fesa.aquela_loja.models.entity.ProductModel;
 import br.edu.fesa.aquela_loja.models.enums.CategoryEnum;
+import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,4 +12,10 @@ public interface IProductRepository extends JpaRepository<ProductModel, Long> {
     boolean existsByName(String name);
 
     List<ProductModel> findByCategory(CategoryEnum category);
+
+    List<ProductModel> findTop10ByCategory(CategoryEnum category);
+
+    List<ProductModel> findByNameContainingIgnoreCase(String name);
+
+    List<ProductModel> findTop10ByOrderByName();
 }
